fd38ecb49303fbe2f4d91e47af9bb16e17003018
DevYeop <lee.sangyeop0715@gmail.com>
2023-11-15 15:09:49 +0900

    Revert "리버트 상황연출"
    This reverts commit cd3954c59267a9bd7b49ee8a1d8cb9c15c490a70.

--- src/main/java/com/devyeop/git5/HomeController.java
+++ src/main/java/com/devyeop/git5/HomeController.java
@@ -42,10 +42,9 @@ public class HomeController {
 		//8888
 		//10-10-10
 		
-		String afterStashString = "스태쉬 꺼내서 다시 커밋하기 좋은 코드로 변경";
-		String stashString = "개발하다가 커밋하긴 참 애매한 상황";
+		String afterStashString = "스태쉬후 커밋하기 좋은 코드상황";
 		
-		String sdfasdfString = "리버트할거임111111111";
+		String sdfasdfString = "리버트";
 		
 		model.addAttribute("serverTime", formattedDate );
 		
